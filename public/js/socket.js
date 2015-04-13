fed08514f7027d0036df4bb7fc5cef7c8230288a
tommasobratto <tbratto22@gmail.com>
2015-04-13 17:01:42 +0100

    basic socket.io setup done

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,3 +1,7 @@
 // this file contains Socket.io client functions
-// var socket = io();
+var socket = io.connect('http://localhost:3000');
 
+socket.on('hello world', function(msg) {
+  console.log(msg);
+  socket.emit('w00t', { txt: 'w00t'})
+});
--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var server = require('http').createServer(app);
 var throng = require('throng');
 var port = process.env.PORT || 3000;
 var WORKERS = process.env.WEB_CONCURRENCY || 1;
-var io = require('socket.io')();
+var io = require('socket.io')(server);
 
-  app.use(express.static('public'));
+app.use(express.static('public'));
 
 throng(start, {
   workers: WORKERS,
@@ -17,6 +17,13 @@ function start() {
 
   app.get('/', function(req, res) {
     res.sendFile('index.html');
+  });
+
+  io.on('connection', function(socket) {
+    socket.emit('hello world', { msg: 'hello world' });
+    socket.on('w00t', function(msg) {
+      console.log(msg);
+    });
   });
 
   server.listen(port, function() {
