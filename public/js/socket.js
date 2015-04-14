4deebdc4619d7f6a432d3da56189c3151651f79f
tommasobratto <tbratto22@gmail.com>
2015-04-14 12:26:03 +0100

    changed index.html script calls

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,5 +1,5 @@
 // this file contains Socket.io client functions
-var socket = io.connect('http://localhost:3000');
+var socket = io();
 
 function connectToServer() {
   socket.on('connect', function(data) {
