53ef2fa82c027128b0ab052b9ec0d54f0deef839
Vanessa <virgitti@gmail.com>
2015-04-14 13:58:16 +0100

    fixed button with routes

--- router/main.js
+++ router/main.js
@@ -6,6 +6,9 @@ module.exports=function(app) {
   app.route('/')
     .get(function(req,res){
     res.render('index.html');
+  })
+  .post(function(req, res){
+    res.send('New Game');
   });
 
   app.get('/game', function(req, res){
@@ -13,6 +16,10 @@ module.exports=function(app) {
   });
 
   app.get('/choose', function(req, res){
+    res.render('choose.html')
+  });
+
+  app.post('/choose', function(req, res){
     res.render('choose.html')
   });
 
